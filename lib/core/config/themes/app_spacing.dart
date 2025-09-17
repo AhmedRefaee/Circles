6b302bf81a41a3ab7c6dfa7b4de14944d3721645
Ahmed Refaee <arefaee390@gmail.com>
2025-09-17 18:45:55 +0300

    feat: add AppSpacing and AppAssets classes for consistent layout and asset management

---
+++ lib/core/config/themes/app_spacing.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppSpacing {
+  /// Outter Spacing for the whole screen
+  static final double screenHorizontal = 30.w;
+  static final double screenVertical = 30.h;
+
+  /// Inner HORIZONTAL Spacing for the whole screen
+  static final double horizontalXSmall = 5.w;
+  static final double horizontalSmall = 10.w;
+  static final double horizontalMedium = 15.w;
+  static final double horizontalLarge = 20.w;
+  static final double horizontalXLarge = 25.w;
+  static final double horizontalXXLarge = 30.w;
+  static final double horizontalXXXLarge = 35.w;
+  static final double horizontalFourty = 40.w;
+  static final double horizontalFifty = 50.w;
+  static final double horizontalSixty = 60.w;
+  static final double horizontalSeventy = 70.w;
+  static final double horizontalEighty = 80.w;
+  static final double horizontalNinety = 90.w;
+  static final double horizontalHundred = 100.w;
+
+  /// Inner VERTICAL Spacing for the whole screen
+  static final double verticalXSmall = 5.h;
+  static final double verticalSmall = 10.h;
+  static final double verticalMedium = 15.h;
+  static final double verticalLarge = 20.h;
+  static final double verticalXLarge = 25.h;
+  static final double verticalXXLarge = 30.h;
+  static final double verticalXXXLarge = 35.h;
+  static final double verticalFourty = 40.h;
+  static final double verticalFifty = 50.h;
+  static final double verticalSixty = 60.h;
+  static final double verticalSeventy = 70.h;
+  static final double verticalEighty = 80.h;
+  static final double verticalNinety = 90.h;
+  static final double verticalHundred = 100.h;
+}
---
+++ lib/core/consts/app_assets.dart
@@ -0,0 +1,5 @@
+class AppAssets {
+  static const String logo = 'assets/images/logo/logo.svg';
+  static const String gooleIcon = 'assets/images/icons/google_icon.svg';
+  static const String facebookIcon = 'assets/images/icons/facebook_icon.svg';
+}
