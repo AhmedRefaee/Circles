6fd9dd76319bc210c08cb22c12b90b69f259bb39
Ahmed Refaee <arefaee390@gmail.com>
2025-09-17 18:40:37 +0300

    feat: implement Google Sign-In functionality and update build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,6 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.2.13676358"
 
-  
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -22,21 +20,36 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.circles"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        getByName("debug") {
+            keyAlias = "myAliasName"
+            keyPassword = "qwerty"
+            storeFile = file("myKeyStoreName.jks")
+            storePassword = "qwerty"
+        }
+        create("release") {
+            keyAlias = "myAliasName"
+            keyPassword = "qwerty"
+            storeFile = file("myKeyStoreName.jks")
+            storePassword = "qwerty"
+        }
+    }
+
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+        getByName("debug") {
             signingConfig = signingConfigs.getByName("debug")
+        }
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
--- lib/features/auth/social_login/presentation/view/google_login_btn.dart
+++ lib/features/auth/social_login/presentation/view/google_login_btn.dart
@@ -1,4 +1,9 @@
+import 'package:circles/core/config/themes/app_spacing.dart';
 import 'package:circles/core/config/themes/app_text_styles.dart';
+import 'package:circles/core/consts/app_assets.dart';
+import 'package:circles/core/utils/testing/test_file.dart';
+import 'package:circles/features/auth/social_login/data/service/google_signin_api.dart';
+import 'package:circles/generated/l10n.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -7,22 +12,42 @@ import 'package:flutter_svg/svg.dart';
 class GoogleLoginBtn extends StatelessWidget {
   const GoogleLoginBtn({super.key});
 
+  Future signIn(BuildContext context) async {
+    final user = await GoogleSigninApi.login();
+
+    if (user == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Sign in failed, please try again.")),
+      );
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => SocialAccInfo(user: user)),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey, width: 0.5),
-        borderRadius: BorderRadius.circular(50.r),
-      ),
-      height: 50.h,
-      width: double.infinity,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset("assets/images/icons/google_icon.svg"),
-          SizedBox(width: 10.w),
-          Text("Continue with Google", style: AppTextStyles.regular),
-        ],
+    return GestureDetector(
+      onTap: () => signIn(context),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey, width: 0.5),
+          borderRadius: BorderRadius.circular(50.r),
+        ),
+        height: 50.h,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(AppAssets.gooleIcon),
+            SizedBox(width: AppSpacing.horizontalSmall),
+            Text(
+              S.of(context).continueWithGoogle,
+              style: AppTextStyles.regular,
+            ),
+          ],
+        ),
       ),
     );
   }
